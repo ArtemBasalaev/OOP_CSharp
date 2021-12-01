0ac420bc64fff41e870a900db857b945ba3a2fd7
ArtBas <art-basalaev@yandex.ru>
2021-12-01 17:27:17 +0700

    Задача "Лямбды"

---
+++ Lambda/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lambda
+{
+    public class Person
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public string Name => _name;
+
+        public int Age => _age;
+
+        public Person(string name, int age)
+        {
+            if (age <= 0)
+            {
+                throw new ArgumentException($"Значение возраста должно быть положительным числом, переданное значение age = {age}");
+            }
+
+            if (name == null)
+            {
+                throw new ArgumentException("Необходимо заполнить поле имя");
+            }
+
+            if (name.Length <= 1)
+            {
+                throw new ArgumentException($"Имя должно состоять из более чем одного символа, сейчас введено символов: {name.Length}");
+            }
+
+            _name = name;
+            _age = age;
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var persons = new LinkedList<Person>(new[]
+            {
+                new Person("Ivan", 25),
+                new Person("Ivan", 13),
+                new Person("Denis", 30),
+                new Person("Sergey", 22),
+                new Person("Pyotr", 12),
+                new Person("Anton", 43)
+            });
+
+            //А
+            var uniqueNamesList = persons
+                .Select(p => p.Name)
+                .Distinct()
+                .ToList();
+
+            //Б
+            var uniqueNamesString = string.Join(", ", uniqueNamesList);
+
+            Console.WriteLine("В списке содержатся следующие уникальные имена:");
+            Console.WriteLine($"Имена: {uniqueNamesString}.");
+
+            //В
+            const int limitAge = 18;
+
+            var personsYoungerThanLimitAge = persons
+                .Where(p => p.Age < limitAge)
+                .ToList();
+
+            var averageAge = personsYoungerThanLimitAge
+                .Select(p => p.Age)
+                .Average();
+
+            Console.WriteLine($"Средний возраст людей младше 18: {averageAge}");
+
+            //Г
+            var averageAgeByName = persons
+                .GroupBy(p => p.Name)
+                .ToDictionary(group => group.Key, group => group.ToList().Select(p => p.Age).Average());
+
+            Console.WriteLine("Средний возраст людей, имеющих одинаковое имя:");
+            Console.WriteLine(string.Join(", ", averageAgeByName));
+
+            //Д
+            const int minAge = 20;
+            const int maxAge = 45;
+
+            var personsInRangeAges = persons
+                .Where(p => p.Age >= minAge && p.Age <= maxAge)
+                .ToList();
+
+            var personsNamesDescendingByAge = personsInRangeAges
+                .OrderByDescending(p => p.Age)
+                .Select(p => p.Name)
+                .ToArray();
+
+            Console.WriteLine($"Список людей возраст, которых находится в диапазоне от {minAge} до {maxAge}, в порядке убывания их возраста: ");
+            Console.Write(string.Join(", ", personsNamesDescendingByAge));
+        }
+    }
+}
